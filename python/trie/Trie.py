5f66c17d3d27466bb5db5505e4acdee5d2d6e669
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-07-06 21:27:00 +0530

    Add Delete

--- python/trie/Trie.py
+++ python/trie/Trie.py
@@ -24,6 +24,28 @@ class Trie:
             current = current.children[key[ind]]
         return (current != None and current.is_end_of_word)
 
+    def remove(self, key):
+        return self.removeUtil(self.root, key, 0)
+
+    def removeUtil(self, root, key, deapth):
+        if(not root):
+            return None
+
+        if(deapth == len(key)):
+            if(root.is_end_of_word):
+                root.is_end_of_word = False
+            if(len(root.children)):
+                root = None
+            return root
+            
+        root.children[key[deapth]] = self.removeUtil(root.children[key[deapth]], key, deapth+1)
+
+        if(len(root.children) == 0 and not root.is_end_of_word):
+            root = None
+        return root
+
+
+
 if __name__ == '__main__':
     trie = Trie()
     keys = ["the", "a", "there", "answer", "any", "by", "bye", "their"]
@@ -31,5 +53,7 @@ if __name__ == '__main__':
     for ele in keys:
         trie.insert(ele)
 
-    print(trie.search("the"))
-    print(trie.search("these"))
+    #print(trie.search("the"))
+    #print(trie.search("these"))
+    trie.remove("there")
+    print(trie.search("there"))
