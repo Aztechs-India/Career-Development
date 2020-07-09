08979e91a72810c6b12375db90704064d4fd2313
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-07-09 09:24:31 +0530

    Code Refactor

--- python/trie/word-boggle.py
+++ python/trie/word-boggle.py
@@ -9,14 +9,15 @@ def match_words_in_dictionary(boggle, result):
         for j in range(len(boggle[i])):
             if(boggle[i][j] in srcs and not children[boggle[i][j]].visited):
                 match_words_util(i, j, children[boggle[i][j]], boggle, boggle[i][j], result)
-    # for src in srcs:
-    #     match_words_util(trie.root.children[src], src, result)
 
-def is_safe(i, j):
+def is_safe(i, j, src):
     global boggle
     row = len(boggle)
     col = len(boggle[0])
-    return i>=0 and i<row and j>=0 and j<col
+    if(i>=0 and i<row and j>=0 and j<col):
+        curr = boggle[i][j]
+        return curr in src.children and not src.children[curr].visited
+    return False
 
 def match_words_util(row, col, src, boggle, part_string, result):
     src.visited = True
@@ -26,9 +27,9 @@ def match_words_util(row, col, src, boggle, part_string, result):
     for i,j in adjacents:
         i = row+i
         j = col+j
-        if(is_safe(i, j) and boggle[i][j] in src.children and not src.children[boggle[i][j]].visited):
-            match_words_util(i, j, src.children[boggle[i][j]], boggle, part_string+boggle[i][j], result)
-        
+        if(is_safe(i, j, src)):
+            curr = boggle[i][j]
+            match_words_util(i, j, src.children[curr], boggle, part_string+curr, result)
 
 words = ['GEEKS', 'FOR', 'QUIZ', 'GO']
 
