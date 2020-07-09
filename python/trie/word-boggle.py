0c5977e688572284f7c46d924b4aadc06b7ebfe8
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-07-09 21:32:00 +0530

    CodeFactor Review Comments Fix

--- python/trie/word-boggle.py
+++ python/trie/word-boggle.py
@@ -1,17 +1,15 @@
 from Trie import Trie
 
-def match_words_in_dictionary(boggle, result):
-    global trie
+def match_words_in_dictionary(boggle, result, trie):
     children = trie.root.children
     srcs = trie.root.children.keys()
 
-    for i in range(len(boggle)):
-        for j in range(len(boggle[i])):
+    for i, v1 in enumerate(boggle):
+        for j, v2 in enumerate(boggle[i]):
             if(boggle[i][j] in srcs and not children[boggle[i][j]].visited):
                 match_words_util(i, j, children[boggle[i][j]], boggle, boggle[i][j], result)
 
-def is_safe(i, j, src):
-    global boggle
+def is_safe(i, j, src, boggle):
     row = len(boggle)
     col = len(boggle[0])
     if(i>=0 and i<row and j>=0 and j<col):
@@ -27,7 +25,7 @@ def match_words_util(row, col, src, boggle, part_string, result):
     for i,j in adjacents:
         i = row+i
         j = col+j
-        if(is_safe(i, j, src)):
+        if(is_safe(i, j, src, boggle)):
             curr = boggle[i][j]
             match_words_util(i, j, src.children[curr], boggle, part_string+curr, result)
 
@@ -45,5 +43,5 @@ for word in words:
     trie.insert(word)
 
 words_in_boggle = []
-match_words_in_dictionary(boggle, words_in_boggle)
+match_words_in_dictionary(boggle, words_in_boggle, trie)
 print(words_in_boggle)
