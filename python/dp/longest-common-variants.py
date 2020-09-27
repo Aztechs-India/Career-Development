2edb1d9aebcf92f632d33795e475fc425ea798a1
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-09-27 22:39:26 +0530

    Add palidromic substring/sequence

--- python/dp/longest-common-variants.py
+++ python/dp/longest-common-variants.py
@@ -7,12 +7,10 @@ def longest_common_subsequence(string1, string2):
                 dp[i][j] = dp[i-1][j-1]+1
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    print("Dp Array", dp)
     return dp[-1][-1]
 
 def longest_common_substring(string1, string2):
     n, m = len(string1), len(string2)
-    print(n, m)
     dp = [[0 for i in range(m+1)] for i in range(n+1)]
     length = float("-inf")
     for i in range(1, n+1):
@@ -22,8 +20,47 @@ def longest_common_substring(string1, string2):
                 length = max(length, dp[i][j])
             else:
                 dp[i][j] = 0
-    print("Dp Array", dp)
     return length
 
-print(longest_common_substring("ABCDGH", "ABCXYZCDGH"))
+def longest_palindromic_subsequence(string):
+    n = len(string)
+    dp = [[0 for i in range(n)] for i in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+    for i in range(1, n):
+        row = 0
+        for j in range(0, n):
+            end = j + i
+            if(end >= n):
+                break
+            if(string[j] == string[end]):
+                dp[row][end] = dp[row+1][end-1]+2
+            else:
+                dp[row][end] = max(dp[row-1][end], dp[row][end-1])
+            row+=1
+    return dp[0][-1]
+
+def longest_palindromic_substring(string):
+    n = len(string)
+    dp = [[0 for i in range(n)] for i in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+    maximum = float("-inf")
+    for i in range(1, n):
+        row = 0
+        for j in range(0, n):
+            end = j + i
+            if(end >= n):
+                break
+            if(string[j] == string[end] and dp[row+1][end-1] == (end-row-1)):
+                dp[row][end] = dp[row+1][end-1]+2
+                maximum = max(maximum, dp[row][end])
+            else:
+                dp[row][end] = 0
+            row+=1
+    return maximum
+#print(longest_common_substring("ABCDGH", "ABCXYZCDGH"))
+#print(longest_palindromic_subsequence("NACAMACAN"))
+print(longest_palindromic_substring("forgeeksskeegfor"))
+
 
